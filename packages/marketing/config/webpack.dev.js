8fd5b9f6739633d418ddbd0ad28f0b9b6287652b
gabriel <gabrielrsp18@gmail.com>
2022-11-21 19:15:32 -0300

    add webpack develop config

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,20 @@
+const { merge } = require('webpack-merge') // usado para mergear configurações em comum de prod e dev
+const HtmlWebpackPlugin = require('html-webpack-plugin') // usado para injeção de html
+const commonConfig = require('./webpack.common')
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
+
+module.exports = merge(commonConfig, devConfig)
