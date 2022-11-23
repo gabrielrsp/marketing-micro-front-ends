f20ca05591b6e529f0b74106841f9ab243288ed1
gabriel <gabrielrsp18@gmail.com>
2022-11-23 20:20:19 -0300

    integration of container and marketing

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge') // usado para mergear configurações em comum de prod e dev
 const HtmlWebpackPlugin = require('html-webpack-plugin') // usado para injeção de html
 const commonConfig = require('./webpack.common')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 const devConfig = {
   mode: 'development',
@@ -11,6 +12,12 @@ const devConfig = {
     }
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+         marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+      }
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html'
     })
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge') // usado para mergear configurações em comum de prod e dev
 const HtmlWebpackPlugin = require('html-webpack-plugin') // usado para injeção de html
 const commonConfig = require('./webpack.common')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 const devConfig = {
   mode: 'development',
@@ -13,6 +14,13 @@ const devConfig = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
+    }),
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      }
     })
   ]
 }
